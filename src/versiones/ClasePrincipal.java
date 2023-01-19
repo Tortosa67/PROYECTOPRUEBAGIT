bcfe84e0b90f1914a7e2daa7fd089fe10267c00f
tortosa67 <tortosa67@msn.com>
2023-01-19 15:44:13 +0100

    Creacion de prueba persona

--- src/versiones/ClasePrincipal.java
+++ src/versiones/ClasePrincipal.java
@@ -1,5 +1,12 @@
 package versiones;
 
 public class ClasePrincipal {
+	
+	public static void main (String[] args) {
+		
+		String nombre ="Sergio";
+		
+		System.out.println("Hola " + nombre);
+	}
 
 }
